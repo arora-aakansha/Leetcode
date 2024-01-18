2f021f58b577136fc555c5ddf368487458eb838b
aakansha <aakansha.a03@gmail.com>
2024-01-18 14:37:15 -0500

    Max_profit_for_stock

---
+++ Max_profit_of_stock.py
@@ -0,0 +1,18 @@
+#Problem Statement
+'''
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sum_of_stock = 0
+        for i in range(0, len(prices)-1):
+            if prices[i+1]>prices[i]:
+                sum_of_stock = sum_of_stock + prices[i+1]-prices[i]
+        return sum_of_stock
+        
+        
